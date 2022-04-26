69a851d6f14ea35a07584a85033f9c6ae5efd469
Dani <daniel.sprenger@vmtecnologia.io>
2022-04-26 12:21:41 -0300

    implementado sed para editar o cmdline.txt

--- config.sh
+++ config.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-sudo -i
 echo "enable_uart=1" >> /boot/config.txt
 echo "dtoverlay=pi3-miniuart-bt" >> /boot/config.txt
 chmod 666 /dev/ttyAMA0
 systemctl stop serial-getty@ttyS0.service
 systemctl disable serial-getty@ttyS0.service
-echo "console=tty1 root=PARTUUID=7b9105cd-02 rootfstype=ext4 fsck.repair=yes rootwait quiet splash plymouth.ignore-serial-consoles" >/boot/cmdline.txt
+sed -i 's/console=serial0,115200 //g' /boot/cmdline.txt
 
 echo "i2c-dev" >> /etc/modules
 echo "i2c-bcm2708" >> /etc/modules
@@ -17,4 +16,3 @@ sudo apt-get install i2c-tools libi2c-dev python-smbus
 
 echo "dtparam=spi=on" >> /boot/config.txt
 
-(crontab -l; echo "@reboot sudo sh /home/pi/gigaSW/main.sh &") | sort -u | crontab -;
