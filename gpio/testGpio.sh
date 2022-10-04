f6dbb28fee6a91ff51263b070716e195446608bb
danielsprengervm <daniel.sprenger@vmtecnologia.io>
2022-10-04 17:35:57 +0100

    algumas alteracoes (que nao deveriam ter sido feitas) para testar o HW da versao 13/06, provavelmente nao serao aplciadas na versao de producao da giga

--- gpio/testGpio.sh
+++ gpio/testGpio.sh
@@ -2,50 +2,54 @@
 
 #Testa a entrada e saída da IF1 e IF2
 
+if1_out="12"
+if1_in="20"
+
+if2_out="13"
+if2_in="27"
+
 #config IF1
-raspi-gpio set 19 ip
-raspi-gpio set 19 pn
-raspi-gpio set 16 op
+raspi-gpio set ${if1_in} ip pn
+raspi-gpio set ${if1_out} op
 #config IF2
-raspi-gpio set 12 ip
-raspi-gpio set 12 pn
-raspi-gpio set 13 op
+raspi-gpio set ${if2_in} ip pn
+raspi-gpio set ${if2_out} op
 
 #teste IF1
-raspi-gpio set 16 dh
-raspi-gpio set 13 dl
-sleep 0.5
-rx_gpio1=$(raspi-gpio get 19)
-rx_gpio2=$(raspi-gpio get 12)
+raspi-gpio set ${if1_out} dh
+raspi-gpio set ${if2_out} dl
+sleep 0.1
+rx_gpio1=$(raspi-gpio get ${if1_in})
+rx_gpio2=$(raspi-gpio get ${if2_in})
 if [[ "${rx_gpio1}" != *"level=1"* || "${rx_gpio2}" != *"level=0"* ]]; then
 	echo "Erro GPIO IF1"
 	exit 1
 fi
 
-raspi-gpio set 16 dl
-raspi-gpio set 13 dl
-sleep 0.5
-rx_gpio1=$(raspi-gpio get 19)
-rx_gpio2=$(raspi-gpio get 12)
+raspi-gpio set ${if1_out} dl
+raspi-gpio set ${if2_out} dl
+sleep 0.1
+rx_gpio1=$(raspi-gpio get ${if1_in})
+rx_gpio2=$(raspi-gpio get ${if2_in})
 if [[ "${rx_gpio1}" != *"level=0"* || "${rx_gpio2}" != *"level=0"* ]]; then
 	echo "Erro GPIO IF1"
 	exit 2
 fi
 #Teste IF2
-raspi-gpio set 16 dl
-raspi-gpio set 13 dh
-sleep 0.5
-rx_gpio1=$(raspi-gpio get 19)
-rx_gpio2=$(raspi-gpio get 12)
+raspi-gpio set ${if1_out} dl
+raspi-gpio set ${if2_out} dh
+sleep 0.1
+rx_gpio1=$(raspi-gpio get ${if1_in})
+rx_gpio2=$(raspi-gpio get ${if2_in})
 if [[ "${rx_gpio1}" != *"level=0"* || "${rx_gpio2}" != *"level=1"* ]]; then
 	echo "Erro GPIO IF2"
 	exit 3
 fi
-raspi-gpio set 16 dl
-raspi-gpio set 13 dl
-sleep 0.5
-rx_gpio1=$(raspi-gpio get 19)
-rx_gpio2=$(raspi-gpio get 12)
+raspi-gpio set ${if1_out} dl
+raspi-gpio set ${if2_out} dl
+sleep 0.1
+rx_gpio1=$(raspi-gpio get ${if1_in})
+rx_gpio2=$(raspi-gpio get ${if2_in})
 if [[ "${rx_gpio1}" != *"level=0"* || "${rx_gpio2}" != *"level=0"* ]]; then
 	echo "Erro GPIO IF2"
 	exit 4
--- userIf/testButtons.sh
+++ userIf/testButtons.sh
@@ -3,10 +3,10 @@
 #Testa botoes SW1, SW2 e leds
 
 #teste botão SW1
-raspi-gpio set 18 ip
-raspi-gpio set 18 pu
+raspi-gpio set 21 ip
+raspi-gpio set 21 pu
 rx_gpio=" "
-rx_gpio=$(raspi-gpio get 18)
+rx_gpio=$(raspi-gpio get 21)
 if [[ "${rx_gpio}" != *"level=1"* ]]; then
 	echo "Falha SW1"
 	exit 1
@@ -18,7 +18,7 @@ startTime=$(date +%s)
 now=$(date +%s)
 while [[ $((now)) -le $((startTime+10)) ]]
 do
-	rx_gpio=$(raspi-gpio get 18)
+	rx_gpio=$(raspi-gpio get 21)
 	if [[ "${rx_gpio}" == *"level=0"* ]]; then
 		success=1
 		break
@@ -31,34 +31,6 @@ if [ $success -eq 0 ]; then
 	exit 2
 fi
 echo "SW1 ok"
-
-#Teste SW2
-raspi-gpio set 24 ip
-raspi-gpio set 24 pu
-rx_gpio=$(raspi-gpio get 24)
-if [[ "${rx_gpio}" != *"level=0"* ]]; then
-	echo "Falha SW2"
-	exit 3
-fi
-
-echo "Pressione SW2"
-success=0
-startTime=$(date +%s)
-now=$(date +%s)
-while [[ $((now)) -le $((startTime+10)) ]]
-do
-	rx_gpio=$(raspi-gpio get 24)
-	if [[ "${rx_gpio}" == *"level=1"* ]]; then
-		success=1
-		break
-	fi
-	now=$(date +%s)
-done
-if [ $success -eq 0 ]; then
-	echo "Falha SW2"
-	exit 4
-fi
-echo "SW2 ok"
 
 exit 0
 
