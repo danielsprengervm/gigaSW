311c5fdd4815a57a5e9fad27135e5d249f570392
danielsprengervm <daniel.sprenger@vmtecnologia.io>
2023-01-19 19:12:37 +0000

    Na versao anterior faltou corrigir o spiComm e o montiro de 5v_sys

--- spi/spiComm.py
+++ spi/spiComm.py
@@ -12,7 +12,7 @@ rcv_byte = spi.xfer2([0x10, 0x02, 0xFF, 0xFF, 0x10, 0x03])
 time.sleep(0.05)
 rcv_byte = spi.xfer2([0,0,0,0,0,0,0,0,0,0,0])
 
-if rcv_byte[0:6] == [16, 2, 255, 86, 1, 1]:
+if rcv_byte[0:6] == [16, 2, 255, 86, 5, 1]:
 	print("Ok")
 else:
 	print("Nok")
